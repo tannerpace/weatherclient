496c0000107045ef1f72c0f0656760a226cc4ccf
TannerB <tannerpace@gmail.com>
2023-03-12 21:01:41 -0400

    adds photos

--- pages/_document.js
+++ pages/_document.js
@@ -2,8 +2,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   const meta = {
-    title: 'Next.js Blog Starter Kit',
-    description: 'Clone and deploy your own Next.js portfolio in minutes.',
+    title: 'Tanner Bleakley - Portfolio',
+    description: 'Tanner Bleakley is a full-stack developer and devops engineer.',
     image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
   }
 
