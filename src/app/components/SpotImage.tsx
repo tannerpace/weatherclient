e51dd109e0d6003a608a224cd3afa99d661a9890
TannerB <tannerpace@gmail.com>
2024-07-04 22:34:00 -0400

    dd

--- src/app/components/SpotImage.tsx
+++ src/app/components/SpotImage.tsx
@@ -1,35 +1,24 @@
 "use client"
 import React from "react"
+import Image from "next/image"
 import { KitesurfSpot } from "../../../mock"
-import Box from "@mui/material/Box"
+
 interface SpotImageProps {
   spot: KitesurfSpot
 }
 
 const SpotImage: React.FC<SpotImageProps> = ({ spot }) => {
   return (
-    <Box
-      sx={{
-        height: "100%",
-        backgroundImage: `url(${spot?.location_img_url})`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        backgroundRepeat: "no-repeat",
-        position: "relative",
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "500px",
-        width: "100%",
-        borderRadius: "10px",
-        boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.6)",
-        backgroundColor: "rgba(0,0,0,0.5)",
-        "&:hover": {
-          boxShadow: "0px 0px 10px rgba(16, 81, 211, 0.5)",
-        },
-      }}
-    />
+    <div className="relative flex flex-col justify-center items-center w-full min-h-[500px] h-full rounded-lg shadow-lg hover:shadow-blue-500 overflow-hidden">
+      <Image
+        src={spot?.location_img_url || "/default-image.jpg"}
+        alt={spot?.name || "Kitesurf Spot"}
+        layout="fill"
+        objectFit="cover"
+        quality={100}
+        className="rounded-lg"
+      />
+    </div>
   )
 }
 
