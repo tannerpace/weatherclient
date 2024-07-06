5acdcd6b36cd152c1793aeb1a1b9d6509baf73b5
TannerB <tannerpace@gmail.com>
2024-07-06 02:44:49 -0400

    pwa

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,12 @@ const nextConfig = {
   compiler: {
     removeConsole: process.env.NODE_ENV !== "development",
   },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development',
+    register: true,
+    skipWaiting: true,
+  },
   async redirects() {
     return [
       {
