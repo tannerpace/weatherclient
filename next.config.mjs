4f9cf160164fa1cfdd32f5e979567de59589b9cd
TannerB <tannerpace@gmail.com>
2024-07-06 02:36:44 -0400

    update

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,22 @@
-import withPWA from 'next-pwa';
-
-const nextConfig = {
+module.exports = withPWA({
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
+    removeConsole: process.env.NODE_ENV !== "development",
   },
-};
-
-export default withPWA({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [
+          {
+            type: "host",
+            value: "weatherclient.vercel.app",
+          },
+        ],
+        permanent: true,
+        destination: "https://windysession.com/:path*",
+      },
+    ];
+  },
+});
