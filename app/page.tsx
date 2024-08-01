deb94c2078545ae31e1fc550de3bb6c810dcb40e
TannerB <tannerpace@gmail.com>
2024-07-31 23:59:32 -0400

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -156,7 +156,7 @@ const Page: React.FC = () => {
             Navigate to Target
           </button>
           <button
-            onClick={handleNavigateToProfile}
+            onClick={() => handleNavigateToProfile}
             className="mt-4 px-4 py-2 bg-yellow-500 text-white rounded flex items-center justify-center hover:bg-green-700 w-full md:w-auto"
           >
             <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
