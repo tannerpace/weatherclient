09e1ac0885cb3badf5ede3bda23e2ba20c8af87c
TannerB <tannerpace@gmail.com>
2024-10-12 16:17:55 -0400

    search smaller

--- components/Search.tsx
+++ components/Search.tsx
@@ -14,20 +14,18 @@ const Search: React.FC<SearchProps> = ({ onSearch }) => {
   }
 
   return (
-    <div className="flex items-center rounded relative">
-      <Input
-        type="text"
-        value={query}
-        onChange={(e: { target: { value: any } }) => {
-          const value = e.target.value
-          setQuery(value)
-          handleSearch(value)
-        }}
-        placeholder="Search for a location"
-        required
-        className="w-full py-2 rounded-lg p-2"
-      />
-    </div>
+    <Input
+      type="text"
+      value={query}
+      onChange={(e: { target: { value: any } }) => {
+        const value = e.target.value
+        setQuery(value)
+        handleSearch(value)
+      }}
+      placeholder="Search for a location"
+      required
+      className="w-full rounded-lg"
+    />
   )
 }
 
