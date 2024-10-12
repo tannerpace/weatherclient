c35adba3b81817be662c4e57933c3735e1eed24f
TannerB <tannerpace@gmail.com>
2024-10-12 16:18:26 -0400

    enum

---
+++ app/enums/ActivityEnum.ts
@@ -0,0 +1,15 @@
+enum ActivityEnum {
+  Kitesurfing = 1,
+  Hiking = 2,
+  Cycling = 3,
+  Fishing = 4,
+  HuntingDuck = 5,
+  HuntingDeer = 6,
+  HuntingTurkey = 7,
+  Running = 8,
+  Surfing = 9,
+  FishingRedfish = 10,
+  ShrimpingWithPoles = 11,
+}
+
+export default ActivityEnum
