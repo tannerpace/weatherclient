f4a215b9b5da1f9cbf136efb2d847eb6db408b3d
TannerB <tannerpace@gmail.com>
2024-10-14 14:55:13 -0400

    lastworking

--- pages/_document.js
+++ pages/_document.js
@@ -3,8 +3,8 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   const meta = {
     title: 'Tanner Bleakley - Portfolio',
-    description: 'Tanner Bleakley is a full-stack developer and devops engineer.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    description: 'Tanner Bleakley is a full-stack developer and DevOps engineer.',
+    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png' // Update this with your actual image URL
   }
 
   return (
@@ -16,8 +16,10 @@ export default function Document() {
         <meta property="og:description" content={meta.description} />
         <meta property="og:title" content={meta.title} />
         <meta property="og:image" content={meta.image} />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://tannerb.dev" />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@yourname" />
+        <meta name="twitter:site" content="@tannerpaceb" />
         <meta name="twitter:title" content={meta.title} />
         <meta name="twitter:description" content={meta.description} />
         <meta name="twitter:image" content={meta.image} />
